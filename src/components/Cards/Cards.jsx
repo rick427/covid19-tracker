0a0e8fdad03639af6346833776e46fa1e4311643
rick427 <rchard@yahoo.com>
2020-04-11 10:17:31 +0100

    project setup, folder structure and component architecture

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cards = () => {
+    return (
+        <div>
+            CARDS
+        </div>
+    )
+}
+
+export default Cards;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CountryPicker = () => {
+    return (
+        <div>
+            COUNTRY PICKER
+        </div>
+    )
+}
+
+export default CountryPicker
