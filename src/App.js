f63410bc63c77326492808b53c6a27fde40fd9d4
rick427 <rchard@yahoo.com>
2020-04-11 10:25:37 +0100

    modifying api util func

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import {Cards, Chart, CountryPicker} from "./components";
 import {fetchData} from './api';
 
 class App extends Component {
+    
     async componentDidMount(){
         const data = await fetchData();
         console.log(data);
     }
+
     render() {
         return (
             <div className={styles.container}>
